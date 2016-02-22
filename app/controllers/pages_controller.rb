2c765a17af7745203e5bd9f3c25860ff9d0f3628
wimp777 <wimp777@gmail.com>
2016-02-22 02:17:20 +0000

    Post view markup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
@@ -14,8 +15,10 @@ class PagesController < ApplicationController
       redirect_to root_path, :notice=> "User not found"
     end
     
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   def explore
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/user/:id' => 'pages#profile'
 
-  get 'explore' => 'pages#explore'
+  get '/explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
