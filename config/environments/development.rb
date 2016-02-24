fd6c3f661d7019c9eaafa03e34b3a2e69071c788
wimp777 <wimp777@gmail.com>
2016-02-24 18:18:04 +0000

    Landing page creation and style

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
   
   #Devise default mailer for Devlopment enviornment
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.web_console.whitelisted_ips = '0.0.0.0/0.0.0.0'
+  
 end
